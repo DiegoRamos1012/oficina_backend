7c32f4e9477bf5061f49a7ee16d9ed80944c4042
diego1012 <diegorsantos09@outlook.com>
2025-06-04 09:36:13 -0300

    Estruturação do projeto, instalação de pacotes iniciais

---
+++ main.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+    "log"
+    "time"
+    
+    "github.com/gin-contrib/cors"
+    "github.com/gin-gonic/gin"
+    
+    "oficina/configs"
+    "oficina/routes"
+)
+
+func main() {
+    // Inicializar o router
+    r := gin.Default()
+    
+    // Configurar CORS
+    r.Use(cors.New(cors.Config{
+        AllowOrigins:     []string{"http://localhost:3000"},
+        AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
+        AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+        ExposeHeaders:    []string{"Content-Length"},
+        AllowCredentials: true,
+        MaxAge:           12 * time.Hour,
+    }))
+    
+    // Carregar configurações
+    config, err := configs.LoadConfig()
+    if err != nil {
+        log.Fatalf("Erro ao carregar configurações: %v", err)
+    }
+    
+    // Configurar rotas
+    routes.SetupRoutes(r)
+    
+    // Iniciar o servidor
+    log.Printf("Servidor iniciado na porta %s", config.ServerPort)
+    r.Run(":" + config.ServerPort)
+}
