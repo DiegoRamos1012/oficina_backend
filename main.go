bfed825c7b5b17a90baf135842b70392c2f8f810
DiegoRamos1012 <diegorsantos09@Outlook.com>
2025-07-19 23:14:32 -0300

    Mudei o CORS pra permitir todas as origens

--- main.go
+++ main.go
@@ -31,12 +31,12 @@ func main() {
 
 	// Configurar CORS
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
 		MaxAge:           12 * time.Hour,
+		AllowAllOrigins: true,
 	}))
 
 	if config.Environment != "production" {
